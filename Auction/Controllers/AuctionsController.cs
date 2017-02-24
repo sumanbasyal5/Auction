c0175cbe29a4991b130a38d04566f75a76fb4a9e
sumanbasyal5 <basyal.suman866@gmail.com>
2017-02-24 17:28:32 -0600

    work on models

---
+++ Auction/Controllers/AuctionsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Auction.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        //
+        // GET: /Auctions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Auction()
+        {
+            return View();
+        }
+
+    }
+}
