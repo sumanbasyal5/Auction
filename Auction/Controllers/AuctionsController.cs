d9a0c5244ef507180a8da951e56fd93eb088f4ce
sumanbasyal5 <basyal.suman866@gmail.com>
2017-02-28 17:24:27 -0600

    work on auction

--- Auction/Controllers/AuctionsController.cs
+++ Auction/Controllers/AuctionsController.cs
@@ -14,11 +14,28 @@ namespace Auction.Controllers
         public ActionResult Index()
         {
             return View();
+            
+        }
+
+        public ActionResult TempDataDemo()
+        {
+            TempData["SuccessMessage"] = "The action succedded";
+            return RedirectToAction("Index");
         }
 
         public ActionResult Auction()
         {
-            return View();
+            var auction = new Auction.Models.Auction()
+            {
+                Title = "Example Auction",
+                Description = "This is an example Auction",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddDays(7),
+                StartPrice = 1.00m,
+                CurrentPrice = null
+            };
+            
+            return View(auction);
         }
 
     }
